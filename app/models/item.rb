f5ddfee904b6a6d627c710bfc59fc98b074fce56
Stephen Lucero <slucero@forumone.com>
2013-01-16 12:19:29 +0000

    Alter models to implement acts_as_list behavior in items.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :list
+  acts_as_list :scope => :list
   attr_accessible :description, :weight
 
   def url_for(options = {})
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
 class List < ActiveRecord::Base
-  has_many :items
+  has_many :items, :order => :position
   attr_accessible :summary, :title
 end
