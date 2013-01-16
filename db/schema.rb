3acbd87fa4142c3b1f8f60b78570bd0f7f7aa27e
Stephen Lucero <slucero@forumone.com>
2013-01-16 12:17:58 +0000

    Add acts_as_list gem and migrate db for compatibility.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112001833) do
+ActiveRecord::Schema.define(:version => 20130116012355) do
 
   create_table "items", :force => true do |t|
     t.string   "description"
-    t.integer  "weight"
+    t.integer  "position"
     t.integer  "list_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
