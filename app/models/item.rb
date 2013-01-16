ebdb575deb2e2d749a65ebaf8ce7c4d6ad2ed8f9
Stephen Lucero <slucero@forumone.com>
2013-01-16 12:55:01 +0000

    Clean up unused url_for method from item model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,4 @@ class Item < ActiveRecord::Base
   belongs_to :list
   acts_as_list :scope => :list
   attr_accessible :description, :weight
-
-  def url_for(options = {})
-    super(options || {:id => id, :list_id => list_id})
-  end
 end
